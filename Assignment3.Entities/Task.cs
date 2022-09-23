673afb7621a99fb8ce2ef9bff02d19a9829a62e9
Selma Bernild <selmabernild@MacBook-Air-tilhrende-Selma.local>
2022-09-23 14:07:45 +0200

    The start of the Entities classes

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -1,5 +1,38 @@
 namespace Assignment3.Entities;
+using System.ComponentModel.DataAnnotations;
 
 public class Task
 {
+    public int Id { get; set; }
+    [StringLength(100), Required]
+    public string Title { get; set; }
+    public User? AssignedTo { get; set; }
+    [StringLength(int.MaxValue)]
+    public string? Description { get; set; }
+    [Required]
+    public State State { get; set; }
+    public virtual ICollection<Tag>? Tags { get; set; }
+
+
+
+    public Task(int id, string t, User? a, string? d, State s, ICollection<Tag>? tag)
+    {
+        this.Id = id;
+        this.Title = t;
+        this.AssignedTo = a;
+        this.Description = d;
+        this.State = s;
+        this.Tags = tag;
+    }
+
+    
+
+}
+
+public enum State {
+    New,
+    Active,
+    Resolved,
+    Closed,
+    Removed
 }
--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -1,5 +1,12 @@
 namespace Assignment3.Entities;
+using System.ComponentModel.DataAnnotations;
 
 public class User
 {
+    public int Id { get; set; }
+    [StringLength(100), Required]
+    public string Name { get; set; }
+    [StringLength(100), Required]
+    public string Email { get; set; }
+    public 
 }
